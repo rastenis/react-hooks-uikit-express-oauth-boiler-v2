8ebf5d6387ef91fb8d29485178fe73c9cce17636
Scharkee <matas2k@gmail.com>
2020-12-20 21:03:28 +0200

    cleanup

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -1,72 +1,11 @@
 import to from "await-to-js";
 import axios from "axios";
 
-export const REQUEST_AUTH = `REQUEST_AUTH`;
-export const PROCESSING_AUTH = `PROCESSING_AUTH`;
-export const AUTHENTICATING = `AUTHENTICATING`;
-export const AUTHENTICATED = `AUTHENTICATED`;
-export const AUTH_ERROR = `AUTH_ERROR`;
-export const SET_DATA = `SET_DATA`;
-export const CLEAR_DATA = `CLEAR_DATA`;
-export const ADD_MESSAGE = `ADD_MESSAGE`;
-export const DELETE_MESSAGE = `DELETE_MESSAGE`;
-export const REQUEST_ACCOUNT_CREATION = `REQUEST_ACCOUNT_CREATION`;
-export const REQUEST_SESSION_FETCH = `REQUEST_SESSION_FETCH`;
-export const REQUEST_LOGOUT = `REQUEST_LOGOUT`;
-export const WAITING = `WAITING`;
-export const REQUEST_AUTH_UNLINK = `REQUEST_AUTH_UNLINK`;
-export const REQUEST_PASSWORD_CHANGE = `REQUEST_PASSWORD_CHANGE`;
-
-export const requestAuth = (email, password) => ({
-  type: REQUEST_AUTH,
-  email,
-  password,
-});
-
-export const processAuth = (status = AUTHENTICATING) => ({
-  type: PROCESSING_AUTH,
-  authenticated: status,
-});
-
-export const requestLogout = () => ({
-  type: REQUEST_LOGOUT,
-});
-
-export const addMessage = (msg) => ({
-  type: ADD_MESSAGE,
-  msg: msg,
-});
-
-export const deleteMessage = (msg) => ({
-  type: DELETE_MESSAGE,
-  msg: msg,
-});
-
-export const setData = (data = {}) => ({
-  type: SET_DATA,
-  data,
-});
-
-export const clearData = () => ({
-  type: CLEAR_DATA,
-});
-
-export const requestAccountCreation = (email, password) => ({
-  type: REQUEST_ACCOUNT_CREATION,
-  email,
-  password,
-});
-
-export const requestPasswordChange = (oldPassword, newPassword) => ({
-  type: REQUEST_PASSWORD_CHANGE,
-  oldPassword,
-  newPassword,
-});
-
-export const requestAuthUnlink = (toUnlink) => ({
-  type: REQUEST_AUTH_UNLINK,
-  toUnlink,
-});
+// use this to inspect axios requests
+// axios.interceptors.request.use(request => {
+//   console.log("Starting Request", request);
+//   return request;
+// });
 
 const handleRequestError = (dispatch, error) => {
   if (!error) {
@@ -223,7 +162,6 @@ export const mainReducer = (state: MainState, action: Action): MainState => {
 };
 
 export enum AuthState {
-  PROCESSING = `PROCESSING`,
   AUTHENTICATING = `AUTHENTICATING`,
   AUTHENTICATED = `AUTHENTICATED`,
   NOT_AUTHENTICATED = `NOT_AUTHENTICATED`,
--- src/client/store/old/sagas.js
+++ src/client/store/old/sagas.js
@@ -2,59 +2,6 @@ import { take, put } from "redux-saga/effects";
 import axios from "axios";
 import * as mutations from "./mutations";
 
-// use this to inspect axios requests
-// axios.interceptors.request.use(request => {
-//   console.log("Starting Request", request);
-//   return request;
-// });
-
-export function* authenticationSaga(context) {
-  while (true) {
-    const { email, password } = yield take(mutations.REQUEST_AUTH);
-    try {
-      const { data } = yield axios.post(`/api/auth`, {
-        email,
-        password
-      });
-      yield put(mutations.setData(data.state));
-      yield put(mutations.processAuth(mutations.AUTHENTICATED));
-      yield put(mutations.addMessage({ msg: data.msg, error: false }));
-
-      yield put({
-        type: mutations.REQUEST_SESSION_FETCH
-      });
-
-      context.routerHistory.push("/");
-    } catch (e) {
-      // TODO: set error message
-      yield put(mutations.processAuth(mutations.AUTH_ERROR));
-      yield put(mutations.addMessage({ msg: e.response.data, error: true }));
-    }
-  }
-}
-
-export function* registrationSaga(context) {
-  while (true) {
-    const { email, password } = yield take(mutations.REQUEST_ACCOUNT_CREATION);
-    try {
-      const { data } = yield axios.post(`/api/register`, {
-        email,
-        password
-      });
-      yield put(mutations.setData(data.state));
-      yield put(mutations.processAuth(mutations.AUTHENTICATED));
-      yield put(mutations.addMessage({ msg: data.msg, error: false }));
-
-      context.routerHistory.push("/");
-    } catch (e) {
-      // TODO: set error message
-      console.log(e);
-      yield put(mutations.processAuth(mutations.AUTH_ERROR));
-      yield put(mutations.addMessage({ msg: e.response.data, error: true }));
-    }
-  }
-}
-
 export function* passwordChangeSaga(context) {
   while (true) {
     const { newPassword, oldPassword } = yield take(
@@ -63,7 +10,7 @@ export function* passwordChangeSaga(context) {
     try {
       const { data } = yield axios.post(`/api/changePassword`, {
         oldPassword,
-        newPassword
+        newPassword,
       });
       yield put(mutations.addMessage({ msg: data.msg, error: false }));
     } catch (e) {
@@ -76,45 +23,12 @@ export function* authUnlinkSaga(context) {
     const { toUnlink } = yield take(mutations.REQUEST_AUTH_UNLINK);
     try {
       const { data } = yield axios.post(`/api/unlink`, {
-        toUnlink
+        toUnlink,
       });
       yield put(mutations.addMessage({ msg: data.msg, error: false }));
       yield put(mutations.setData(data.state));
     } catch (e) {
       yield put(mutations.addMessage({ msg: e.response.data, error: true }));
     }
-  }
-}
-
-export function* sessionFetchSaga(context) {
-  while (true) {
-    yield take(mutations.REQUEST_SESSION_FETCH);
-    try {
-      const { data } = yield axios.get(`/api/data`);
-      yield put(mutations.setData(data.state));
-      yield put(
-        mutations.processAuth(data.auth ? mutations.AUTHENTICATED : null)
-      );
-
-      // register server side messages, if any
-      if (Object.keys(data.message || {}).length > 0) {
-        yield put(mutations.addMessage(data.message));
-      }
-      context.routerHistory.push("/");
-    } catch (e) {
-      // no conncetion to backend
-    }
-  }
-}
-
-export function* logoutSaga(context) {
-  while (true) {
-    yield take(mutations.REQUEST_LOGOUT);
-    try {
-      yield axios.post(`/api/logout`);
-      yield put(mutations.clearData()); // removing user data, but keeping messages & auth state
-      yield put(mutations.processAuth(null));
-      context.routerHistory.push("/");
-    } catch (e) {}
   }
 }
