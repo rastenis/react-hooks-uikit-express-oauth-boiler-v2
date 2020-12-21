6c2b3c5b7cb90834e200c81c4146ba2aab823ce2
Scharkee <matas2k@gmail.com>
2020-12-21 13:15:37 +0200

    package updates and cleanup + removed greenlock

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,26 +2,8 @@
 
 import app from "./server";
 import config from "../../config/config.json";
-import greenlock from "greenlock-express";
-import greenlockStore from "greenlock-store-fs";
 
-// TLS auto-setup if selfHosted is true in the config
-if (config.selfHosted) {
-  greenlock
-    .create({
-      email: config.tls.email,
-      agreeTos: config.tls.tos,
-      approveDomains: config.tls.domains,
-      configDir: "~/.config/acme/",
-      store: greenlockStore,
-      app: app,
-      //debug: true
-    })
-    .listen(80, 443);
-  console.log(`Server listening on ports 80 and 443`);
-} else {
-  // http app listening
-  let port = config.port || process.env.PORT || 7777;
-  app.listen(port);
-  console.log(`Server listening on port ${port}`);
-}
+// http app listening
+let port = config.port || process.env.PORT || 7777;
+app.listen(port);
+console.log(`Server listening on port ${port}`);
